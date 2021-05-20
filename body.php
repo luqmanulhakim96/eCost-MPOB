1adefeed9c5ce8cb800f613b6d1b9e05b01df023
Syed <syeltzeen@gmail.com>
2021-05-20 13:53:47 +0800

    Update body.php

--- body.php
+++ body.php
@@ -10,7 +10,7 @@ include ('../Connections/connection.class.php');
 include('../setstring.inc');
 
 // Turn off all error reporting
-error_reporting(0);
+error_reporting(1);
 extract($_REQUEST);
 
 $_SESSION['tahun'] = $tahun;
